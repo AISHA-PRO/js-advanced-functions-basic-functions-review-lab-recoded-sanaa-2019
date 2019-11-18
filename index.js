9addc569edbced0fccfc020ff31c95220790414a
Aisha Rashed <aisha1rashed@gmail.com>
2019-11-18 13:46:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+  return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(activity = "go to the office"){
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(str = "*"){
+  return function(adj = "special"){
+    return `You are ${str}${adj}${str}!`;
+  }
+
+}
+let Calculator={
+  add : function(num1 , num2){
+    return num1+num2;
+  },
+  subtract : function(num1 , num2){
+    return num1-num2;
+  },
+  multiply : function(num1 , num2){
+    return num1-num2;
+  },
+  divide : function(num1 , num2){
+    return num1-num2;
+  }
+}
+
+let encouragingPromptFunction = wrapAdjective("!!!");
+wrapAdjective("%")("a dedicated programmer") //=> "You are %a dedicated programmer%!"
